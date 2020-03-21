00533d3d66a551d56f15b5c71ecde240137fd0ab
juliekim520 <julie.kim520@gmail.com>
2020-03-21 22:04:38 +1100

    dynamic cache

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = 'site-static-v1';
+const dynamicCache = 'site-dynamic-v1';
 const assets = [
   '/',
   '/index.html',
@@ -44,7 +45,15 @@ self.addEventListener('fetch', (evt) => {
   // console.log('fetch event', evt);
   evt.respondWith(
     caches.match(evt.request).then((cacheRes) => {
-      return cacheRes || fetch(evt.request);
+      return (
+        cacheRes ||
+        fetch(evt.request).then((fetchRes) => {
+          return caches.open(dynamicCache).then((cache) => {
+            cache.put(evt.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
     })
   );
 });
